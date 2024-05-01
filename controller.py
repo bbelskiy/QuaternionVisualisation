e8f6bf6e1d4ea21439b09d2dbef95222514bd6d9
Богдан Коржак <korzhak-bm@dvluch.loc>
2024-05-01 18:02:03 +0300

    changed methods of controller class

--- controller.py
+++ controller.py
@@ -18,6 +18,8 @@ class Controller:
         self.gyr_error = np.array([0, 0, 0], dtype=np.float64)
         self.counter = 0
 
+        self.compensated_gyr_data = np.array([0, 0, 0], dtype=np.float64)
+
         self.calc_gyr_error()
 
     def get_q(self):
@@ -34,22 +36,25 @@ class Controller:
         self.gyr_error /= self.counter
         print(self.gyr_error, self.counter)
 
+    def update_view(self):
+        self.view.set_acc_gyr_data(self.data_model.acceleration, self.compensated_gyr_data, self.q.to_numpy())
+        self.view.qw.setValue(self.q.w)
+        self.view.qx.setValue(self.q.x)
+        self.view.qy.setValue(self.q.y)
+        self.view.qz.setValue(self.q.z)
+
     def run(self):
         while True:
-            self.lock.acquire()
             self.serial_model.read_data()
             self.serial_model.process_data()
             self.data_model.parse(self.serial_model.get_processed_data())
-            ang_vel = (self.data_model.angular_velocity - self.gyr_error) / 57.4
-            self.qW.set_vector_as_q(ang_vel)
 
-            q = self.q * self.qW / 2
-            self.q += q * 0.01
-            self.q.normalize()
+            self.calculate_orientation()
+            self.update_view()
 
-            self.view.set_acc_gyr_data(self.data_model.acceleration, ang_vel, self.q.to_numpy())
-            self.view.qw.setValue(self.q.w)
-            self.view.qx.setValue(self.q.x)
-            self.view.qy.setValue(self.q.y)
-            self.view.qz.setValue(self.q.z)
-            self.lock.release()
+    def calculate_orientation(self):
+        self.compensated_gyr_data = self.data_model.angular_velocity - self.gyr_error
+        self.qW.set_vector_as_q(self.compensated_gyr_data)
+        q = self.q * self.qW / 2
+        self.q += q * 0.012
+        self.q.normalize()
